6c11e62d20a5e37e78dd601d9a1ed778fef8c3df
vinculando_intellij <git@github.com:SandovalBrandon1027/vinculando_intellij.git>
2023-11-27 22:17:15 -0500

    Completando la API de memes

--- API_memes/colorChange.js
+++ API_memes/colorChange.js
@@ -4,8 +4,7 @@ const bg= ["#ff3381","#93f","#f56","#eeff00","#003cff"]
 const fg=["#111","#fff","#111","#111","fff"]
 
 function colorChance(){
-    let limit=bg.length
-    let index = Math.floor(Math.random() * limit);
-    memeBtn.style.background=bg[index]
-    memeBtn.style.color=fg[index]
+  
 }
+
+colorChance()
